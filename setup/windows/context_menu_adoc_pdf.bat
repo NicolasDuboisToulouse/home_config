3285f598cfbb0869ecf6631ff63d7aca41e73d57
Nicolas Dubois <NicolasDuboisToulouse@users.noreply.github.com>
2017-04-25 16:17:31 +0200

    New asciidoc command

--- setup/windows/context_menu_adoc_pdf.bat
+++ setup/windows/context_menu_adoc_pdf.bat
@@ -1,4 +1,4 @@
-@set COMMAND=C:\local\asciidoctor\bin\ascii2pdf.bat
+@set COMMAND=c:\local\Ruby23-x64\bin\asciidoctor-pdf -b pdf -a pdf-style=%%HOME%%\.config\asciidoctor\snoopy.yml
 
-@powershell %~dp0\context_menu_add.ps1 -file .adoc -action_name asciidoctor_pdf -action_cmd %COMMAND% -action_label 'Generate PDF'
+@powershell %~dp0\context_menu_add.ps1 -file .adoc -action_name asciidoctor_pdf -action_cmd '%COMMAND%' -action_label 'Generate PDF'
 
