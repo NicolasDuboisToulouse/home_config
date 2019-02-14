15e5f40923a7ef1d9bd2b45482edb0fff7ec7d2d
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2019-02-14 10:08:22 +0100

    mklink

---
+++ emacs/win32_link_dotEmacs.bat
@@ -0,0 +1,4 @@
+@echo executer CMD en admin avec la commande :
+@echo mklink %HOME%\.emacs %HOME%\.config\emacs\dot_emacs
+
+@pause
--- emacs/win32_update_dotEmacs.bat
+++
@@ -1,5 +0,0 @@
-rem del %HOME%\.emacs
-rem copy %HOME%\.config\emacs\dot_emacs %HOME%\.emacs
-echo "Utiliser mklink en tant qu'admin" 
-
-pause
