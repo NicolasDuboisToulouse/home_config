b246d231325e311228a566a503f88c12c139ac7f
Nicolas Dubois <NicolasDuboisToulouse@users.noreply.github.com>
2019-09-26 13:31:22 +0200

    Using .profile not nos works all the time. switch to .bashrc

--- setup/linux/create_home_links.sh
+++ setup/linux/create_home_links.sh
@@ -1,4 +1,5 @@
+set -x
 ln -s ~/.config/emacs/dot_emacs ~/.emacs
-ln -s ~/.config/shell/bashrc ~/.bashrc
-ln -s ~/.config/shell/profile ~/.profile
-
+ln -s ~/.config/shell/bashrc    ~/.bashrc
+ln -s ~/.config/shell/profile   ~/.profile
+ln -s ~/.config/shell/profile   ~/.bash_profile
