9a9c373b13762ea295a72a9eda16ac3d446c12db
Nicolas Dubois <DuboisNicolasToulouse@users.noreply.github.com>
2021-11-16 13:49:59 +0100

    inputrc: Completion ignore case

--- setup/linux/create_home_links.sh
+++ setup/linux/create_home_links.sh
@@ -3,4 +3,5 @@ ln -s ~/.config/emacs/dot_emacs   ~/.emacs
 ln -s ~/.config/shell/bashrc      ~/.bashrc
 ln -s ~/.config/shell/profile     ~/.profile
 ln -s ~/.config/shell/profile     ~/.bash_profile
+ln -s ~/.config/shell/inputrc     ~/.inputrc
 ln -s ~/.config/git/config_linux  ~/.gitconfig
